b9451cb806e751993482c66fd184d81265837c3b
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-05-07 17:06:54 +1200

    add joinus

--- frontend/components/Toggle.jsx
+++ frontend/components/Toggle.jsx
@@ -1,10 +1,16 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLocale } from '@/components/LocaleContext'
 
 export default function ToggleSwitch() {
   const { isUK, toggleLocale } = useLocale()
+  const [mounted, setMounted] = useState(false)
 
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
   // const [isUK, setIsUK] = useState(false)
 
   // const toggleSwitch = () => {
--- frontend/components/ui/GoogleMapEmbedUK.jsx
+++ frontend/components/ui/GoogleMapEmbedUK.jsx
@@ -11,7 +11,7 @@ const GoogleMapEmbed = ({ size = 'big' }) => {
   return (
     <div className="google-map-container pt-0 ">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4725.65086726355!2d-0.024255323532990536!3d51.46142767180277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487602686b932d75%3A0xae2b6bb7ea8fffa2!2s54%20Fossil%20Rd%2C%20London%20SE13%207DE%2C%20UK!5e1!3m2!1sen!2snz!4v1746591374627!5m2!1sen!2snz"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4971.643652134803!2d-0.024255323532990536!3d51.46142767180277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487602686b932d75%3A0xae2b6bb7ea8fffa2!2s54%20Fossil%20Rd%2C%20London%20SE13%207DE%2C%20UK!5e0!3m2!1sen!2snz!4v1746591516241!5m2!1sen!2snz"
         width={width}
         height={height}
         style={{ border: '0' }}
